f27e8543bed4ae100679e78589ed86ba057155e1
Romina De Vasquez <rominadevasquez@gmail.com>
2026-05-09 13:16:29 -0400

    validacion de formulario

--- scrypt.js
+++ scrypt.js
@@ -1,3 +1,20 @@
 <!--arreglo vacio para crear usuarios--> 
 
 let usuarios = [];
+
+<!--validamos los campos que se ingresan en el formulario creado en index--> 
+
+function validarFormulario(nombre, edad, rol){
+
+    if(nombre === "" || edad === "" || rol === ""){
+        alert("Todos los campos son obligatorios");
+        return false;
+    }
+
+    if(edad <= 0){
+        alert("La edad debe ser mayor a 0");
+        return false;
+    }
+
+    return true;
+}
